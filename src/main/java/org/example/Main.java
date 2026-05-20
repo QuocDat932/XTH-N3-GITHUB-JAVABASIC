35fe8ed18db91977fbb267fd10ce9017d6f4fbaf
QuocDat932 <datbui185@gmail.com>
2026-05-20 13:26:06 +0700

    Commit01: Setup SourceBase Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Xin chào các bạn sinh viên XTH - N03");
+        System.out.println("Lớp: Git/Github - JavaBasic");
+        System.out.println("Giảng viên: Thầy Bùi Quốc Đạt");
+    }
+}
