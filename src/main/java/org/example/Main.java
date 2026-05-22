def7fa871fda6888dfec9ec2dd2a96323b69d0d1
QuocDat932 <datbui185@gmail.com>
2026-05-22 12:59:52 +0700

    Commit02: Update new source - add new function print

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,23 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Xin chào các bạn sinh viên XTH - N03");
-        System.out.println("Lớp: Git/Github - JavaBasic");
-        System.out.println("Giảng viên: Thầy Bùi Quốc Đạt");
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Xin chào các bạn sinh viên XTH - N03");
+        list.add("Lớp: Git/Github - JavaBasic");
+        list.add("Giảng viên: Thầy Bùi Quốc Đạt");
+        list.add("Task01: New Function Print");
+        printMessage(list);
+    }
+
+    public static void printMessage(List<String> list){
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+            System.out.println("-----------------");
+        }
     }
 }
